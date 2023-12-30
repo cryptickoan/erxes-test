978326b3a217ad52b7033614369968f452ec6bbd
Koan <80938373+cryptickoan@users.noreply.github.com>
2023-12-29 17:05:29 -0800

    test

--- src/components/erxes-analytics.tsx
+++ src/components/erxes-analytics.tsx
@@ -11,6 +11,11 @@ const ErxesAnalytics = () => (
       id="erxes-analytics"
       dangerouslySetInnerHTML={{
         __html: `
+        window.erxesSettings = {
+            messenger: {
+              brand_id: "lrHF17",
+            },
+          }
             var script = document.createElement('script');
           script.src = "https://guatemedica.app.erxes.io/widgets/build/messengerWidget.bundle.js";
           script.async = true;
