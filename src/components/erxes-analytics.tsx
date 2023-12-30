9b8f6304d74112a4c49af01559a70d301af78cf9
Koan <80938373+cryptickoan@users.noreply.github.com>
2023-12-29 17:54:04 -0800

    idek

--- src/components/erxes-analytics.tsx
+++ src/components/erxes-analytics.tsx
@@ -11,16 +11,16 @@ const ErxesAnalytics = () => (
       id="erxes-analytics"
       dangerouslySetInnerHTML={{
         __html: `
-        window.erxesSettings = {
-            messenger: {
-              brand_id: "lrHF17",
-            },
-          }
-            var script = document.createElement('script');
-          script.src = "https://guatemedica.app.erxes.io/widgets/build/messengerWidget.bundle.js";
-          script.async = true;
-          var entry = document.getElementsByTagName('script')[0];
-          entry.parentNode.insertBefore(script, entry);
+         window.erxesSettings = {
+    messenger: {
+      brand_id: "GEQE6C",
+    },
+  };
+        var script = document.createElement('script');
+  script.src = "https://test-pkos4.app.erxes.io/widgets/build/messengerWidget.bundle.js";
+  script.async = true;
+  var entry = document.getElementsByTagName('script')[0];
+  entry.parentNode.insertBefore(script, entry);
         `,
       }}
     ></Script>
