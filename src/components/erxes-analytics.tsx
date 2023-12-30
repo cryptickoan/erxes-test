42987a70047e071b8e97777612170d69c9cb2d71
Koan <80938373+cryptickoan@users.noreply.github.com>
2023-12-29 16:53:11 -0800

    test

---
+++ src/components/erxes-analytics.tsx
@@ -0,0 +1,25 @@
+import Script from "next/script";
+
+const ErxesAnalytics = () => (
+  <>
+    <Script
+      async
+      src={"https://guatemedica.app.erxes.io/widgets/build/messengerWidget.bundle.js"} 
+      id="lrHF17"
+    ></Script>
+    <Script
+      id="erxes-analytics"
+      dangerouslySetInnerHTML={{
+        __html: `
+            var script = document.createElement('script');
+          script.src = "https://guatemedica.app.erxes.io/widgets/build/messengerWidget.bundle.js";
+          script.async = true;
+          var entry = document.getElementsByTagName('script')[0];
+          entry.parentNode.insertBefore(script, entry);
+        `,
+      }}
+    ></Script>
+  </>
+);
+export default ErxesAnalytics;
+
---
+++ src/components/layout/navbar.tsx
@@ -0,0 +1,6 @@
+export default function Navbar () {
+    return (
+        <div className="bg-[red] w-full h-[20vh]">
+        </div>
+    )
+}
