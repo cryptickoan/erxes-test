e461fba13a4459857e2870b99c7657bd9cb0426c
Koan <80938373+cryptickoan@users.noreply.github.com>
2023-12-29 18:00:43 -0800

    let hope

--- src/components/erxes-analytics.tsx
+++ src/components/erxes-analytics.tsx
@@ -5,22 +5,17 @@ const ErxesAnalytics = () => (
     <Script
       async
       src={"https://guatemedica.app.erxes.io/widgets/build/messengerWidget.bundle.js"} 
-      id="GEQE6C"
+      id="lrHF17"
     ></Script>
     <Script
       id="erxes-analytics"
       dangerouslySetInnerHTML={{
         __html: `
-         window.erxesSettings = {
-    messenger: {
-      brand_id: "GEQE6C",
-    },
-  };
-        var script = document.createElement('script');
-  script.src = "https://test-pkos4.app.erxes.io/widgets/build/messengerWidget.bundle.js";
-  script.async = true;
-  var entry = document.getElementsByTagName('script')[0];
-  entry.parentNode.insertBefore(script, entry);
+            var script = document.createElement('script');
+          script.src = "https://guatemedica.app.erxes.io/widgets/build/messengerWidget.bundle.js";
+          script.async = true;
+          var entry = document.getElementsByTagName('script')[0];
+          entry.parentNode.insertBefore(script, entry);
         `,
       }}
     ></Script>
